610fe22f69d546450834580eee4336b2fdf54977
Wojciech Szymczyk <wojciech.szymczyk@swmansion.com>
2022-07-25 15:32:31 +0200

    Add checking state of transactions added manually

--- indexer/src/view/infinite.ts
+++ indexer/src/view/infinite.ts
@@ -8,7 +8,7 @@ import {Index, PrimaryColumn, ViewColumn, ViewEntity} from "typeorm";
                (event.content -> 'generation')::numeric          "gameGeneration",
                (event.content -> 'state')::numeric               "gameState",
                (event.content -> 'cell_index')::numeric          "revivedCellIndex",
-               block.status                                      "txStatus",
+               transaction.status                                "txStatus",
                (
                   case
                       when (event.content -> 'state')::numeric=0 then true
@@ -17,8 +17,8 @@ import {Index, PrimaryColumn, ViewColumn, ViewEntity} from "typeorm";
                 ) as "gameExtinct",
                event."createdAt"                                 "createdAt"
         from event
-            left join block
-                on event."blockHash" = block.hash
+            left join transaction
+                on event."txHash" = transaction.hash
         where (event.name='game_evolved'
                AND (event.content -> 'game_id')::numeric = 39132555273291485155644251043342963441664)
                OR event.name='cell_revived';
